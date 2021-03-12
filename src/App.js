ea7c9f57986c31d5b2fa80e3334f7e0b9961a6a6
Joseph M <jmedina1985@hotmail.com>
2021-03-11 23:00:21 -0800

    Working on app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import data from './data';
 import SingleQuestion from './Question';
 function App() {
   const [questions, setQuestions] = useState(data);
-  return <main>accordion project setup</main>;
+  return (
+    <main>
+      <div className="container"></div>
+    </main>
+  );
 }
 
 export default App;
